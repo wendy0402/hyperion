cc0f26bba429245e1a05f5560ad4a11c879ed6e8
wendy0402 <wendykurniawan92@gmail.com>
2016-08-26 23:23:50 +0700

    cleaning up code in electron main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var ipcMain = electron.ipcMain;
-var Kafka = require('no-kafka');
 
 var mainWindow = null;
 
@@ -34,39 +33,10 @@ var startupOpts = {
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow(startupOpts);
-
-    if (process.env.NODE_ENV === 'dev') {
-        mainWindow.webContents.on('did-start-loading', function() {
-            mainWindow.webContents.executeJavaScript('var script = document.createElement(\'script\');script.type = \'text/javascript\';script.src=\'http://localhost:35729/livereload.js\';document.body.appendChild(script);');
-        });
-    }
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
     mainWindow.show();
-
-    ipcMain.on('send-kafka', function(event, params){
-      if(typeof params === 'undefined'){
-        event.sender.send('kafka-result', {success: false, error: 'params not defined'})
-      }
-      var producer = new Kafka.Producer({connectionString: params['connectionString'], clientId: params['clientId']});
-
-      producer.init().then(function(){
-        return producer.send({
-            topic: params['topic'],
-            partition: params['partition'],
-            message: {
-                value: params['message']
-            }
-        });
-      })
-      .then(function (result) {
-        /*
-        [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
-        */
-        event.sender.send('kafka-result', {success: true, result: result})
-      });
-    });
 });
